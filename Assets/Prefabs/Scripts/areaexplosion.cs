c886a64f358934ff6e1206046fa33d3e44d00c33
DESKTOP-TBSN9A2\raule <raulero2002@gmail.com>
2023-05-16 22:27:45 +0200

    puertas e intento explosion

---
+++ Assets/Prefabs/Scripts/areaexplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areaexplosion : MonoBehaviour
+{
+    public GameObject jugador;
+    public VidaJugador vidaJugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaJugador = jugador.GetComponent<VidaJugador>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        //Evitamos que al chocar con el suelo desaparezca el objeto, para que solo lo haga con el jugador
+        if (other.gameObject.tag == "Player")
+        {
+            vidaJugador.recibirDaño(30.0f);
+        }
+    }
+}
---
+++ Assets/Prefabs/Scripts/puerta.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puerta : MonoBehaviour
+{
+    public GameObject jugador;
+    private float distanciaObjetivo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaObjetivo = 2.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            float distancia = Vector3.Distance(transform.position, jugador.transform.position);
+            if (distancia <= distanciaObjetivo)
+            {
+                Debug.Log("Ahora se abriria la puerta");
+                StartCoroutine(AbrirPuerta());
+            }
+        }
+    }
+
+    private IEnumerator AbrirPuerta()
+    {
+        int i = 0;
+
+        while(i < 200)
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0015f, 0.0f);
+            yield return new WaitForSeconds(0.0035f);
+            i++;
+        }
+        Destroy(gameObject);
+    }
+}
