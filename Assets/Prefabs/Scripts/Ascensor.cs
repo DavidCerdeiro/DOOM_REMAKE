af65c9b33e0a060e57afb8166c98a1fe965ffc3a
DESKTOP-TBSN9A2\raule <raulero2002@gmail.com>
2023-05-22 22:28:27 +0200

    ascensor

---
+++ Assets/Prefabs/Scripts/Ascensor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ascensor : MonoBehaviour
+{
+    //Vector3(-9.39000034,9.81000042,20.0300007)
+    //Inicial: Vector3(-9.39000034,-0.100000001,20.0300007)
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
